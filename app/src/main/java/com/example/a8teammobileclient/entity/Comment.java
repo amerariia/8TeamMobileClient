6afcbc78998b1a5c6081b1c79fd4b45af8bbed69
amerariia <polyanska.nastya@gmail.com>
2021-02-13 00:47:15 +0200

    Add new fields

--- app/src/main/java/com/example/a8teammobileclient/entity/Comment.java
+++ app/src/main/java/com/example/a8teammobileclient/entity/Comment.java
@@ -1,4 +1,10 @@
 package com.example.a8teammobileclient.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Comment {
+    String text;
 }
--- app/src/main/java/com/example/a8teammobileclient/entity/Group.java
+++ app/src/main/java/com/example/a8teammobileclient/entity/Group.java
@@ -1,4 +1,18 @@
 package com.example.a8teammobileclient.entity;
 
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Group {
+    Integer id;
+    String name;
+    Group parentGroup;
+    List<User> pupils;
+    List<User> teachers;
+    List<Task> tasks; //Posts
+    List<String> usefulLinks;
 }
