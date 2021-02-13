d2ebd2cb4ce47453218313db6041bf37b6ce90f6
amerariia <polyanska.nastya@gmail.com>
2021-02-13 17:11:06 +0200

    Create UI for authentication

---
+++ app/src/main/java/com/example/a8teammobileclient/authentication/AuthenticationActivity.java
@@ -0,0 +1,17 @@
+package com.example.a8teammobileclient.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a8teammobileclient.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+    }
+}
---
+++ app/src/main/java/com/example/a8teammobileclient/authentication/RegistrationFragment.java
@@ -0,0 +1,4 @@
+package com.example.a8teammobileclient.authentication;
+
+public class RegistrationFragment {
+}
