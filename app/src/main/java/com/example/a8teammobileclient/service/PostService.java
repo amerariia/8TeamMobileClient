449b8de6e0bed401ac6be9f61684d82249d55afe
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2021-02-18 00:44:00 +0200

    Create service for post due to swagger

---
+++ app/src/main/java/com/example/a8teammobileclient/service/PostService.java
@@ -0,0 +1,28 @@
+package com.example.a8teammobileclient.service;
+
+import com.example.a8teammobileclient.entity.Post;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+
+public interface PostService {
+    @POST("Post")
+    Call<ResponseBody> create();
+
+    @GET("Post")
+    Call<List<Post>> get();
+
+    @GET("Post/{id}")
+    Call<Post> get(@Path("id") Integer id);
+
+    @DELETE("Post/{id}")
+    Call<ResponseBody> delete(@Path("id") Integer id);
+
+    @PUT("Post/{id}")
+    Call<ResponseBody> modify(@Path("id") Integer id);
+
+    @POST("Post/{postId}/add-comment")
+    Call<ResponseBody> addComment(@Path("pathId") Integer id);
+}
